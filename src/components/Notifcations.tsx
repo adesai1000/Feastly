a27795fd9d21784f58b94f29ae4b21973469660d
Ayush <ayushdesai1000@gmail.com>
2024-05-12 18:29:49 +0530

    Persist Elements and Notif

---
+++ src/components/Notifcations.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Notifcations = () => {
+  return (
+    <div className='h-12 bg-chick-red text-white px-4 flex items-center justify-center text-center md:text-base'>Free Delivery for orders above $50!  <span className='hover:underline cursor-pointer ml-1'> Order Now</span></div>
+  )
+}
+
+export default Notifcations

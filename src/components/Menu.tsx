ae4db7e6c933095afbcac510b51baea0b99ca55c
Ayush <ayushdesai1000@gmail.com>
2024-05-20 18:49:12 +0530

    Navbar

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react'
 import { RxHamburgerMenu } from "react-icons/rx";
 import { MdClose } from "react-icons/md";
 import Link from 'next/link';
-import CartIcon from './CartIcon';
 
 
 const links = [
@@ -24,16 +23,15 @@ const Menu = () => {
             <MdClose className='text-chick-red size-6' onClick={()=>setOpen(false)}/>
         )}
         
-    <div className='bg-chick-red text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10 pt-10 uppercase'>
+    {open&&(<div className='bg-chick-red text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10 pt-10 uppercase'>
         {links.map((item)=>(
             <Link href={item.url} key={item.id} onClick={()=>setOpen(false)}>{item.title}</Link>
         ))}
-        {!user?(<Link href='/login'>Login</Link>
+        {!user?(<Link href='/login' onClick={()=>setOpen(false)}>Login</Link>
         ):(
-        <Link href='/orders'>Orders</Link>)}
-        <Link href="/cart"><CartIcon /></Link>
-
-    </div>
+        <Link href='/orders' onClick={()=>setOpen(false)}>Orders</Link>)}
+        <Link href='/cart' onClick={()=>setOpen(false)}>Cart(3)</Link>
+    </div>)}
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,42 @@
-import React from 'react'
-import Menu from './Menu'
-import Link from 'next/link'
-import Image from 'next/image'
+import React from 'react';
+import Menu from './Menu';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar = () => {
+    const user = false;
     return (
-        <div className='h-14 text-chick-red p-4 flex justify-between items-center shadow-md'>
+        <div className='h-14 text-chick-red p-4 flex justify-between items-center shadow-md lg:px-20 xl:px-40'>
+            {/* LEFT LINKS */}
+            <div className='hidden md:flex gap-4 flex-1 font-semibold '>
+                <Link href='/'>Home</Link>
+                <Link href='/menu'>Menu</Link>
+                <Link href='/contact'>Contact</Link>
+            </div>
             {/* Logo
-            Mobile Menu */}
+            Mobile LOGO */}
             <Link href="/" className='pt-1 pb-1 pl-1 sm:hidden'>
-            <Image src="/Chick-fil-A-Logo.png" alt='Chick-Fill-A Logo' height={80} width={80}/>
+                <Image src="/Chick-fil-A-Logo.png" alt='Chick-Fill-A Logo' height={80} width={80}/>
             </Link>
-            
-            <Link href="/" className='pt-1 pb-1 pl-1 hidden md:block'>
-            <Image src="/Chick-fil-A-Logo.wine.svg" alt='Chick-Fill-A Logo' height={120} width={120}/>
-            </Link>
-            <div>
+            {/* PC LOGO */}
+            <div className='flex-1 flex justify-center'>
+                <Link href="/" className='pt-1 pb-1 pl-1 hidden md:block'>
+                    <Image src="/Chick-fil-A-Logo.wine.svg" alt='Chick-Fill-A Logo' height={120} width={120}/>
+                </Link>
+            </div>
+            {/* Mobile Menu */}
+            <div className='md:hidden'>
                 <Menu />
             </div>
+            {/* RIGHT LINKS */}
+            <div className='hidden md:flex gap-4 flex-1 justify-end font-semibold'>
+                {!user ? (<Link href='/Login'>Login</Link>) : (
+                    <Link href='/order'>Orders</Link>
+                )}
+                <Link href='/contact'>Cart</Link>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;

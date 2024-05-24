e741967d52faab5427b4293cb215c878341eaebf
Ayush <ayushdesai1000@gmail.com>
2024-05-24 20:50:24 +0530

    Footer fix, Slider

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,16 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-chick-red text-white py-8 md:py-12 lg:py-16 px-6 md:px-10 lg:px-20">
+        <footer className="bg-chick-red text-white py-4 md:py-6 lg:py-2 px-6 md:px-10 lg:px-20">
             <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
-                <div className="flex items-center justify-center md:justify-start mb-6 md:mb-0">
-                    <img src="/Chick-fill-A-Logo-White.png" alt="Chick-Fil-A Logo" className="w-28 md:w-24 mr-4" />
-                    <p className="text-sm md:text-base">© All Rights Reserved by CFA Properties, Inc.</p>
+                <div className="flex items-center justify-center md:justify-start md:mb-0">
+                    <Image src="/Chick-fill-A-Logo-White.png" alt="Chick-Fil-A Logo" height={150} width={150} />
+                    <p className="text-sm md:text-base mt-5 ml-5">© All Rights Reserved by CFA Properties, Inc.</p>
                 </div>
                 <nav className="text-sm md:text-base">
-                    <ul className="flex space-x-6">
+                    <ul className="flex space-x-6 md:ml-5 md:mt-5">
                         <li><a href="#" className="hover:text-gray-300">About Us</a></li>
                         <li><a href="#" className="hover:text-gray-300">Menu</a></li>
                         <li><a href="#" className="hover:text-gray-300">Contact</a></li>
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,17 +1,37 @@
+'use client'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
+
+const data = [
+  {
+    id:1,
+    title: "Eat Mor Chikin!",
+    image: "/slide1.png"
+  },
+  {
+    id:2,
+    title: "You're my Chick-fill-A!",
+    image: "/slide2.png"
+  },
+  {
+    id:1,
+    title: "CHICK-FILL-AAAAH!",
+    image: "/slide3.png"
+  },
+]
 
 const Slider = () => {
+  const [currentSlider, setCurrentSlider] = useState(0)
   return (
-    <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]'>
+    <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] md:flex-row'>
         {/* TEXT CONTAINER */}
         <div className="h-1/2 flex items-center justify-center flex-col gap-8 text-chick-red">
-            <h1 className='text-5xl font-bold'>Eat Mor Chikin!</h1>
-            <button className='bg-chick-red text-white p-5 rounded-full text-xl font-bold hover:bg-red-600'>Order Now!</button>
+            <h1 className='text-5xl font-bold text-center'>Eat Mor Chikin!</h1>
+            <button className='bg-chick-red text-white p-5 rounded-full text-xl font-bold hover:bg-[#e51736 ee]'>Order Now!</button>
         </div>
         {/* IMAGE CONTAINER */}
         <div className="w-full h-1/2 relative">
-            <Image src='/slide3.jpeg' width={1000} height={1000} alt='HomePage Image' className='object-cover'/>
+            <Image src='/slide3.jpeg' width={800} height={800} alt='HomePage Image' className='object-cover'/>
         </div>
     </div>
   )

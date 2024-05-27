14d5724d9b8cfc327c5d567d6828d83333329aa1
Ayush <ayushdesai1000@gmail.com>
2024-05-27 18:07:11 +0530

    Featured Page

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { featuredProducts } from "@/data";
+import Image from "next/image";
+import React from "react";
 
 const Featured = () => {
   return (
-    <div>Featured</div>
-  )
-}
+    <div className="w-screen overflow-x-scroll text-chick-red">
+      {/* WRAPPER */}
+      <div className="text-4xl font-bold text-chick-red p-5">Best Sellers</div>
+      <div className="w-max flex">
+        {/* SINGLE ITEM */}
+        {featuredProducts.map((item) => (
+          <div
+            key={item.id}
+            className="w-screen h-[60vh] flex flex-col items-center justify-around p-4   md:w-[50vw] xl:w-[33vw] xl:h-[90vh]"
+          >
+            {/* IMAGE CONTAINER */}
+            {item.img && (
+              <div className="relative flex-1 w-full cursor-pointer">
+                <Image src={item.img} alt="" fill className="object-contain" />
+              </div>
+            )}
+            {/* TEXT CONTAINER */}
+            <div className=" flex-1 flex flex-col items-center justify-center text-center gap-4 pt-5">
+              <h1 className="text-3xl font-bold uppercase xl:text-2xl 2xl:text-3xl ">{item.title}</h1>
+              <p className="p-2 2xl:p-8 text-start font-bold">{item.desc}</p>
+              <span className="text-xl font-bold">${item.price}</span>
+              <button className="bg-chick-red text-white p-4 rounded-full font-bold">
+                Add to Cart
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Featured
+export default Featured;
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,37 +1,47 @@
 'use client'
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const data = [
   {
     id:1,
     title: "Eat Mor Chikin!",
-    image: "/slide1.png"
+    image: "/slide1.jpeg"
   },
   {
     id:2,
     title: "You're my Chick-fill-A!",
-    image: "/slide2.png"
+    image: "/slide2.jpeg"
   },
   {
-    id:1,
+    id:3,
     title: "CHICK-FILL-AAAAH!",
-    image: "/slide3.png"
+    image: "/slide3.jpeg"
   },
 ]
 
 const Slider = () => {
-  const [currentSlider, setCurrentSlider] = useState(0)
+  // const [currentSlide, setCurrentSlide] = useState(0)
+  // useEffect(()=>{
+  //   const interval = setInterval(()=>{
+  //   setCurrentSlide(prev=>(prev === data.length -1 ? 0 : prev+1))
+  //   },2000)
+  //   return ()=> clearInterval(interval)
+  // },[])
   return (
-    <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] md:flex-row'>
+    <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row'>
         {/* TEXT CONTAINER */}
-        <div className="h-1/2 flex items-center justify-center flex-col gap-8 text-chick-red">
-            <h1 className='text-5xl font-bold text-center'>Eat Mor Chikin!</h1>
-            <button className='bg-chick-red text-white p-5 rounded-full text-xl font-bold hover:bg-[#e51736 ee]'>Order Now!</button>
+        <div className="flex-1 flex items-center justify-center flex-col gap-8 text-chick-red px-4 md:px-10 lg:px-10">
+            <h1 className='text-5xl font-bold text-center p-4 md:p-10 lg:p-10'>You're my Chick-fill-A!</h1>
+            <button className='bg-chick-red text-white px-8 py-4 rounded-full text-xl font-bold transition-transform transform hover:scale-105'>
+                Order Now!
+            </button>
         </div>
         {/* IMAGE CONTAINER */}
-        <div className="w-full h-1/2 relative">
-            <Image src='/slide3.jpeg' width={800} height={800} alt='HomePage Image' className='object-cover'/>
+        <div className="w-full flex-1 relative lg:h-full lg:w-1/2 flex items-center justify-center p-4">
+            <div className='relative w-full h-full lg:mt-14 rounded-xl overflow-hidden shadow-lg'>
+                <Image src='/slide2.jpeg' layout='fill' objectFit='cover' alt='HomePage Image' className=''/>
+            </div>
         </div>
     </div>
   )

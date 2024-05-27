41ce27cd81f0e30ac758e26f1b7bd340bd7a168b
Ayush <ayushdesai1000@gmail.com>
2024-05-27 22:18:01 +0530

    Bug Fixes

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -6,12 +6,12 @@ const Offer = () => {
     <div className="bg-black h-screen flex flex-col md:flex-row md:justify-between md:bg-[url('/offerBg.png')] md:h-[70vh]">
       {/* TEXT CONTAINER */}
       <div className="flex-1 flex flex-col justify-center items-center text-center gap-8 p-6">
-        <h1 className="text-white text-5xl font-bold xl:text-6xl">Delicious Burger & French Fry</h1>
-        <p className="text-white xl:text-xl">
-          Progressively simplify effective e-toilers and process-centric methods
-          of empowerment. Quickly pontificate parallel.
+        <h1 className="text-white text-3xl font-bold xl:text-5xl text-left">Explore the full menu
+</h1>
+        <p className="text-white xl:text-xl text-left">
+        Whether you're hungry for a Chick-fil-A® Chicken Sandwich or salads prepared fresh daily, we're here to serve you delicious food made with quality ingredients every day (except Sunday).
         </p>
-        <button className="bg-chick-red text-white rounded-full font-bold py-3 px-6">Order Now</button>
+        <button className="bg-chick-red text-white rounded-full font-bold py-3 px-6">Find Something to eat</button>
       </div>
       {/* IMAGE CONTAINER */}
       <div className="flex-1 w-full relative md:h-full">

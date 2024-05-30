45b9dec89abe19d7fb415fa11f84e17c4aaeaa75
Ayush <ayushdesai1000@gmail.com>
2024-05-30 21:50:11 +0530

    Padding fix

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -5,20 +5,26 @@ import React from "react";
 
 const CategoryPage = () => {
   return (
-    <div className="flex flex-wrap text-chick-red">
+    <div className="flex flex-wrap text-chick-red -m-2 pt-4 pb-4 pl-4 pr-2">
       {pizzas.map((item) => (
-        <Link className="w-full h-[60vh] border-r-2 border-b-2 border-chick-red sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group hover:bg-gray-50 hover:shadow" href={`/product/${item.id}`} key={item.id}>
+        <Link
+          className="w-full h-[calc(60vh-1rem)] border-2 border-chick-red sm:w-[calc(50%-1rem)] lg:w-[calc(33.33%-1rem)] p-4 flex flex-col justify-between group hover:bg-gray-50 hover:shadow m-2"
+          href={`/product/${item.id}`}
+          key={item.id}
+        >
           {/* IMAGE CONTAINER */}
           {item.img && (
             <div className="relative h-[80%]">
-              <Image src={item.img} alt="" fill className="object-contain"/>
+              <Image src={item.img} alt="" fill className="object-contain" />
             </div>
           )}
           {/* TEXT CONTAINER */}
           <div className="flex items-center justify-between font-bold">
-            <h1 className="text-2xl uppercase p-2">{item.title}</h1>
+            <h1 className="text-lg uppercase p-2">{item.title}</h1>
             <h2 className="group-hover:hidden text-xl">${item.price}</h2>
-            <button className="hidden group-hover:block uppercase bg-chick-red text-white p-2 rounded-md">Add to Cart</button>
+            <button className="hidden group-hover:block uppercase bg-chick-red text-white p-2 rounded-md">
+              Add to Cart
+            </button>
           </div>
         </Link>
       ))}
